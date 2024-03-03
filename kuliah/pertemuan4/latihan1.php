ae82d1b1b93926b2c32f8bf367fd8305d222a4d7
apriaja123 <you@example.com>
2024-03-03 16:52:47 +0700

    tugas 3

---
+++ kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,18 @@
+<?php 
+echo date("l,d F Y");
+echo "<br>";
+
+echo time()+86400;
+echo "<br>";
+
+echo time();
+echo "<br>";
+
+echo date("l",time()+86400*100);
+
+echo "<br>";
+echo mktime(0,0,0,2,27,2024);
+echo "<br>";
+echo date ("l", mktime(0,0,0,2,27,2024));
+
+?>
